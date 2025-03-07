e0fe353b13e8a3da96fb4cf19c35451a453ffa0b
Carlos Castro <carlosarturo.castrocastro@gmail.com>
2025-03-07 18:31:06 -0500

    Agregado archivo README

---
+++ README.md
@@ -0,0 +1,2 @@
+# csharpapigenerica3
+API en C# creada con ASP.NET Core
--- obj/Debug/net8.0/csharpapigenerica3.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharpapigenerica3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharpapigenerica3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dfa32d1652534a2ef3eafb3f24b7c219fc7ad96")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharpapigenerica3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharpapigenerica3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
